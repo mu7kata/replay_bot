14b60c150c36404d2b4cb5fb2a9bfe15c19b6d27
munakatayuki <munakata.yuki@pathcreate.co.jp>
2023-04-19 08:48:42 +0900

    Vueのインストールと表示まで

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 require('./bootstrap');
+
+import { createApp } from 'vue';
+import ExampleComponent from './components/ExampleComponent.vue';
+
+const app = createApp({});
+app.component('example-component', ExampleComponent);
+app.mount('#app');
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Hello, {{ name }}!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'example-component',
+    data() {
+        return {
+            name: 'world'
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
